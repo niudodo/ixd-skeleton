3fc69e48eccda8755c39702ec5a138de4581f4e2
shujing zhang <shz310@ucsd.edu>
2019-02-28 00:26:55 -0800

    change navbar,header, navbar colorchange added

---
+++ public/js/navbarColor.js
@@ -0,0 +1,35 @@
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+  initializePage();
+})
+var pagePath=window.location.pathname;
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+  console.log("navbar js connected");
+  if(pagePath=="/home"){
+    $("#homeNav").css("background-color","#C7CBD1" );
+    $("#profileNav").css("background-color","#EDEDED" );
+    $("#statisticsNav").css("background-color","#EDEDED");
+    $("#adviceNav").css("background-color","#EDEDED");
+  }
+  if(pagePath=="/profile"){
+    $("#homeNav").css("background-color","#EDEDED" );
+    $("#profileNav").css("background-color","#C7CBD1" );
+    $("#statisticsNav").css("background-color","#EDEDED");
+    $("#adviceNav").css("background-color","#EDEDED");
+  }
+  if(pagePath=="/statistics"){
+    $("#homeNav").css("background-color","#EDEDED" );
+    $("#profileNav").css("background-color","#EDEDED" );
+    $("#statisticsNav").css("background-color","#C7CBD1");
+    $("#adviceNav").css("background-color","#EDEDED");
+  }
+  if(pagePath=="/advices"){
+    $("#homeNav").css("background-color","#EDEDED" );
+    $("#profileNav").css("background-color","#EDEDED" );
+    $("#statisticsNav").css("background-color","#EDEDED");
+    $("#adviceNav").css("background-color","#C7CBD1");
+  }
+}
