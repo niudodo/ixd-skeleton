25cdb169b574c2fc5256d59a20060c1a3cda8e46
shujing zhang <shz310@ucsd.edu>
2019-02-28 23:37:23 -0800

    graph bug fix

--- public/js/graph.js
+++ public/js/graph.js
@@ -6,7 +6,9 @@ function initializePage(){
 	console.log("graph javascript connected ");
 	google.charts.load('current', {'packages':['corechart']});
   google.charts.setOnLoadCallback(drawWeekChart);
-  google.charts.setOnLoadCallback(drawDetailChart);
+  if(window.location.pathname=='/statistics'){
+    google.charts.setOnLoadCallback(drawDetailChart);
+  }
 }
 
 function drawWeekChart() {
