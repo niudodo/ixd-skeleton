a3dba4beb4183af4b1e0dd647768f7a3a831be4b
shujing zhang <shz310@ucsd.edu>
2019-02-28 19:59:17 -0800

    advice submitted buttom

--- public/js/adviceDelete.js
+++ public/js/adviceDelete.js
@@ -4,13 +4,22 @@ $(document).ready(function(){
 
 function initializePage(){
 	console.log("javascript connected ");
-	$("#NoToLastAdvice").click(buttonClick);
-	$("#completeLastAdvice").click(buttonClick);
+	/*$("#NoToLastAdvice").click(buttonClick);
+	$("#completeLastAdvice").click(buttonClick);*/
+	$("#submitBtnDelete").click(submitClick);
+	$("#submitBtnDidIt").click(submitClick);
 }
 
 function buttonClick(e){
-	e.preventDefault();
+	//e.preventDefault();
 	console.log(this);
 	$("#logTitle").text("You Logged: Nothing yet");
 	$("#lastAdvice").text("NOTHING");
+}
+function submitClick(e){
+	e.preventDefault();
+	console.log(this);
+	$("#contentNotCompleteAdvice").hide();
+	$("#contentCompleteAdvice").hide();
+	buttonClick();
 }
