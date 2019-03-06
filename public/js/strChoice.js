920a820120abfb863738b9bab103e33c4a372925
shujing zhang <shz310@ucsd.edu>
2019-03-05 20:34:08 -0800

    story1 fixed

--- public/js/strChoice.js
+++ public/js/strChoice.js
@@ -1,46 +1,46 @@
 var datafrnd =  [
 			{	image: '../images/friend3.png',
 				story: "Your friend randomly starts crying", 
-				choice1: "Cry with her",
-				choice2: "Ask them if anything is wrong",
-				choice3: "Remain serious and ignore her",
-				choice4: "Tell her to stop crying and suck it up"}, 
+				choice1: "Cheer them up",
+				choice2: "Give them a hug",
+				choice3: "Cry with them",
+				choice4: "Tell them to stop crying and suck it up"}, 
 			{
 				image: "../images/friend4.png",
 				story: "Your friend is ignoring your texts", 
-				choice1: "Start crying",
+				choice1: "Find something fun to do to destress",
 				choice2: "Ask them if anything is wrong",
-				choice3: "Let it go and wait",
+				choice3: "They always do it. Let it go.",
 				choice4: "Call and yell at them"
 			},
 			{
 				image: "../images/2019-02-07.png",
 				story: "Your friend is being sassy and you don't know why",
-				choice1: "Be sassy back",
-				choice2: "Ask her if anything is wrong",
-				choice3: "Don't react",
+				choice1: "Be sassy back as a joke and laugh",
+				choice2: "Make a joke to break the tension",
+				choice3: "Don't react and hold back tears",
 				choice4: "Tell her she's a bad friend"
 			},{	
 				image: "../images/friend5.png",
 				story: "Your friend is complaining to you about the same thing over and over", 
-				choice1: "Say 'can you be quiet for even a sec??'",
-				choice2: "Listen quietly",
-				choice3: "Help her with the issue and give advice",
+				choice1: "Ask them to bowl to get their mind off it",
+				choice2: "Listen patiently",
+				choice3: "Tell them they don't ask about your issues",
 				choice4: "Roll your eyes and say 'this again?'"
 			},{	
 				image: "../images/friend1.png", 
 				story: "Your friend wants to go to a restaurant you don't like much", 
-				choice1: "Go with her unwillingly",
-				choice2: "Ask to go somewhere else",
+				choice1: "Go! You may find something new!",
+				choice2: "She wants it so ofcourse I'll go",
 				choice3: "Tell her yes but you want to choose next time",
 				choice4: "Tell her she has bad taste"
 			},{
 				image: "../images/friend6.png",
 				story: "Your friend is being mean to your common friend", 
-				choice1: "Be mean as well",
-				choice2: "Tell her to not be mean" ,
-				choice3: "Start reading shakespeare out loud",
-				choice4: "Punch her"			}];
+				choice1: "Read shakespeare out loud so they laugh",
+				choice2: "Talk to them both and help work out things" ,
+				choice3: "Let things play out. It's not worth it.",
+				choice4: "Take sides and punch one of them"		}];
 
 var datafam = [
 			{	image: "/images/Family1.png",
